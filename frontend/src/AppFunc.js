9cac5d4213458e830c80372a7234b8fbb39b3cf5
Fitri Handayani <fitriihndynii@gmail.com>
2020-12-03 22:56:23 +0700

    Pengerjaan App Func

--- frontend/src/AppFunc.js
+++ frontend/src/AppFunc.js
@@ -1,64 +1,118 @@
 import React, { useState } from "react";
 
 import List from "./components/List";
+import Empty from "./components/EmptyState";
 import listMovies from "movies.json";
 import "./App.css";
+import {Switch} from "antd";
+
 
 /**
-* Building React component using functional programming paradigm
+ * Building React component using functional programming paradigm
 */
 
 function App() {
-// Btw, this is hooks. useState function returns an array
-// contains the state and a function to set the state -> [state, setState].
-// What you see below is array destruction.
-// Let say you have an array const arr = ["aaa", "bbb"], to access the item
-// we can use index arr[0] OR destruct it like below
-// const [varName, index1] = arr, variable varName is guaranteed to get the value of index 0 OR "aaa"
-// here is the illustration for this situation
-// below is the return value of useState
-// [favItems, setFavItems] = [state, setState]
-    
     const [favItems, setFavItems] = useState(() => []);
-    
+    const [checked, setChecked]  = useState(()=> false);
+    function handleChange(checked){
+        setChecked(checked);
+    }
     function handleItemClick(item) {
-        // immutability
+        //immutability
         const newItems = [...favItems];
         const newItem = { ...item };
-        // Find index of item with id
+
+        //Find index of item with id
         const targetInd = newItems.findIndex(it => it.id === newItem.id);
-        
+
         if (targetInd < 0) newItems.push(newItem);
         else newItems.splice(targetInd, 1); // delete 1 item at index targetInd
-        
-        // trigger to set a new state
+
+        //trigger to set a new state
         setFavItems(newItems);
+    }
+    function handleAddFav(item) {
+        //immutability
+        const newItems = [...favItems];
+        const newItem = { ...item };
+
+        //Find index of item with id
+        const targetInd = newItems.findIndex(it => it.id === newItem.id);
+
+        if (targetInd < 0) newItems.push(newItem);
+        // else newItems.splice(targetInd, 1); // delete 1 item at index targetInd
+
+        //trigger to set a new state
+        setFavItems(newItems);
+    }
+    function deleteListFavorites(){
+        setFavItems([]);
     }
 
     return (
         <div className="container-fluid">
             <h1 className="text-center mt-3 mb-0">Favorites Movie App</h1>
             <p className="text-center text-secondary text-sm font-italic">
-            (This is a <strong>function-based</strong> application)
+                (This is a <strong>function-based</strong> application)
             </p>
+            <div className ="text-center container-fluid align-items-center">
+                <Switch
+                    className ="react-switch"
+                    onChange={handleChange}
+                    checked = {checked}
+                /><p><strong>Show Favorites</strong></p>
+            </div>
             <div className="container pt-3">
+          <b> {checked ? 
             <div className="row">
                 <div className="col-sm">
-                <List
+                  <List
                     title="List Movies"
                     items={listMovies}
-                    onItemClick={handleItemClick}
-                />
+                    onItemClick={handleAddFav}
+                    />
                 </div>
                 <div className="col-sm">
-                <List
+                  <List 
                     title="My Favorites"
                     items={favItems}
                     onItemClick={handleItemClick}
-                />
+                    />
+                    {favItems.length > 0 &&  <button type="buttton" onClick={deleteListFavorites}>Delete All Favorites</button> }  
+                    {favItems.length==0 && <Empty></Empty>} 
                 </div>
-            </div>
-            </div>
+              </div>
+                  :
+                    <div className="row">
+                      <List
+                        title="List Movies"
+                        items={listMovies}
+                        onItemClick={handleAddFav}
+                        />
+                    </div>    }
+            </b>       
+        </div>
+            {/* <div className="container pt-3">
+                <div className="row">
+                    <div className="col-sm">
+                        <List
+                            title="List Movies"
+                            items={listMovies}
+                            onItemClick={handleAddItem}
+                        />
+                    </div>
+                    <div className="col-sm">
+                        <List
+                            title="My Favorites"
+                            items={favItems}
+                            onItemClick={handleItemClick}
+                        />
+                         {favItems.length > 0 &&  <button type="buttton" onClick={deleteAll}>Delete All Favorites</button> }  
+                         {favItems.length==0 && <Empty></Empty>} 
+
+                    </div>
+                </div>
+            </div> */}
         </div>
     );
 }
