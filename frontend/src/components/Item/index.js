079862497e1536b97eacb0f3408513a1156114ed
Fitri Handayani <fitriihndynii@gmail.com>
2020-12-03 18:33:20 +0700

    feat: Tanpa checkbox

--- frontend/src/components/Item/index.js
+++ frontend/src/components/Item/index.js
@@ -21,12 +21,12 @@ export default function Item(props){
                     <div className="d-flex justify-content-between align-items-center">
                         <h5 className="mb-1 font-weight-bold">
                             {judul} ({tahun})
-                            <input
+                            {/* <input
                                 className="ml-2"
                                 type="checkbox"
                                 checked={checked}
                                 onChange={handleChange}
-                            />
+                            /> */}
                         </h5>
                     </div>
                     <p className="font-weight-bold mb-0 text-md">Rating: {rating} / 10</p>
